d4702ac763d1d884ad723cb746bc946cc88fb748
mattiapomelli <mattiapomelli@gmail.com>
2022-09-19 09:27:37 +0200

    Refactor storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,73 @@
+const path = require("path");
+
 module.exports = {
-  stories: [
-    "../components/**/*.stories.mdx",
-    "../components/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "storybook-addon-next",
+    // "storybook-addon-next",
+    {
+      /**
+       * Fix Storybook issue with PostCSS@8
+       * @see https://github.com/storybookjs/storybook/issues/12668#issuecomment-773958085
+       */
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
   ],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
   },
-  staticDirs: ["../public"],
+  webpackFinal: (config) => {
+    /**
+     * Add support for alias-imports
+     * @see https://github.com/storybookjs/storybook/issues/11989#issuecomment-715524391
+     */
+    config.resolve.alias = {
+      ...config.resolve?.alias,
+      "@components": path.resolve(__dirname, "../src/components"),
+      "@providers": path.resolve(__dirname, "../src/providers"),
+      "@layouts": path.resolve(__dirname, "../src/layouts"),
+      "@hooks": path.resolve(__dirname, "../src/hooks"),
+      "@icons": path.resolve(__dirname, "../src/icons"),
+      "@utils": path.resolve(__dirname, "../src/utils"),
+      "@api": path.resolve(__dirname, "../src/api"),
+      "@state": path.resolve(__dirname, "../src/state"),
+    };
+
+    /* Fix issues with util package not being resolved */
+    config.resolve.fallback = {
+      ...config.resolve?.fallback,
+      util: false,
+    };
+
+    /**
+     * Add support for svgr
+     */
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test(".svg")
+    );
+    fileLoaderRule.exclude = /\.svg$/;
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgo: false,
+            icon: true,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,68 +1,4 @@
-import { NextUIProvider, createTheme } from "@nextui-org/react";
-import { Provider } from "jotai";
-import AuthProvider from "@providers/AuthProvider";
-import AuthGuard from "../guards/AuthGuard";
-import "@utils/i18n";
-const myDarkTheme = createTheme({
-  type: "dark",
-  theme: {
-    colors: {
-      // brand colors
-      background: "#1d1d1d",
-      // "purple"
-      purple1: "#fefcfe",
-      purple2: "#fdfaff",
-      purple3: "#f9f1fe",
-      purple4: "#f3e7fc",
-      purple5: "#eddbf9",
-      purple6: "#e3ccf4",
-      purple7: "#d3b4ed",
-      purple8: "#be93e4",
-      purple9: "#8e4ec6",
-      purple10: "#8445bc",
-      purple11: "#793aaf",
-      purple12: "#2b0e44",
-
-      // "yellow"
-      yellowA1: "#abab05",
-      yellowA2: "#ffdd02",
-      yellowA3: "#ffea01",
-      yellowA4: "#ffe601",
-      yellowA5: "#fcdb00",
-      yellowA6: "#f2c900",
-      yellowA7: "#e3b200",
-      yellowA8: "#ebbc00",
-      yellowA9: "#f5d800",
-      yellowA10: "#f7ce00",
-      yellowA11: "#926600",
-      yellowA12: "#291c00",
-
-      primaryLight: "$purple5",
-      primaryLightHover: "$purple6", // commonly used on hover state
-      primaryLightActive: "$purple7", // commonly used on pressed state
-      primaryLightContrast: "$purple9", // commonly used for text inside the component
-      primary: "$purple9",
-      primaryBorder: "$purple9",
-      primaryBorderHover: "$purple10",
-      primarySolidHover: "$purple11",
-      primarySolidContrast: "$white", // commonly used for text inside the component
-      primaryShadow: "$purple8",
-
-      secondaryLight: "$yellowA4",
-      secondaryLightHover: "$yellowA5", // commonly used on hover state
-      secondaryLightActive: "$yellowA6", // commonly used on pressed state
-      secondaryLightContrast: "$yellowA8", // commonly used for text inside the component
-      secondary: "$yellowA8",
-      secondaryBorder: "$yellowA8",
-      secondaryBorderHover: "$yellowA9",
-      secondarySolidHover: "$yellowA10",
-      secondarySolidContrast: "$black", // commonly used for text inside the component
-      secondaryShadow: "$yellowA7",
-    },
-    space: {},
-    fonts: {},
-  },
-});
+import "../src/styles/globals.css";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -73,17 +9,3 @@ export const parameters = {
     },
   },
 };
-
-export const decorators = [
-  (Story) => (
-    <Provider>
-      <NextUIProvider theme={myDarkTheme}>
-        <AuthProvider>
-          <AuthGuard>
-            <Story />
-          </AuthGuard>
-        </AuthProvider>
-      </NextUIProvider>
-    </Provider>
-  ),
-];
