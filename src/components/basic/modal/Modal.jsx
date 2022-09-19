bc00a432f71e1d033898076459325dfc77fb07aa
mattiapomelli <mattiapomelli@gmail.com>
2022-09-19 09:36:06 +0200

    Give focus ring to modal close button

--- src/components/basic/modal/Modal.jsx
+++ src/components/basic/modal/Modal.jsx
@@ -38,7 +38,10 @@ const Modal = ({ show, onClose, title, children, closable = true }) => {
                 "bg-gray-900 relative max-w-sm inline-block w-full py-6 px-5 sm:px-6 align-middle transition-all transform border-gray-500 rounded-2xl mb-10"
               )}
             >
-              <button className="absolute top-2 right-2" onClick={onClose}>
+              <button
+                className="focus-visible:outline-none focus-visible:ring-2 ring-primary ring-opacity-50 rounded-lg absolute top-2 right-2"
+                onClick={onClose}
+              >
                 <CloseIcon className="w-6 h-6 text-gray-500" />
               </button>
               {title && (
