21efb47f15cf5aa22bc95008d6fa02aa2b0cb789
mattiapomelli <mattiapomelli@gmail.com>
2022-09-24 15:28:18 +0200

    Get liked posts when user is loaded

--- src/components/UserLikedSongs.tsx
+++ src/components/UserLikedSongs.tsx
@@ -2,19 +2,24 @@ import { useQuery } from "@tanstack/react-query";
 
 import { User } from "@api/users";
 import { Spinner } from "@components/Basic/Spinner";
-import { useApiClient } from "@providers/AuthProvider";
+import { useApiClient, useUser } from "@providers/AuthProvider";
 
 import { SongCard } from "./Song/SongCard";
 
 export const UserLikedSongs = ({ user }: { user: User }) => {
   const apiClient = useApiClient();
+  const { loading } = useUser();
 
   const {
     data: likedPosts,
     isLoading,
     refetch,
-  } = useQuery(["likedPosts", user?._id], () =>
-    apiClient.users.likes(user.username).then((data) => data.data)
+  } = useQuery(
+    ["likedPosts", user?._id],
+    () => apiClient.users.likes(user.username).then((data) => data.data),
+    {
+      enabled: !loading,
+    }
   );
 
   if (isLoading) return <Spinner />;
