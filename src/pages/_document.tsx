03312c18b9c48cb4fd9e5968b48abd59a1e8b7db
valdozzz <edvaldo99@icloud.com>
2022-10-03 22:40:22 +0200

    added favicon + seo (only index)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="/logo_only.png" />
       </Head>
       <body>
         <Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,32 @@
+import { NextSeo } from "next-seo";
+
 import { Feed } from "@components/Feed";
 
 export default function Home() {
-  return <Feed />;
+  return (
+    <>
+      <NextSeo
+        title="Leets"
+        description="Condividi le tue scoperte musicali e promuovi artisti emergenti"
+        canonical="https://www.leets.it/"
+        openGraph={{
+          url: "https://www.leets.it",
+          title: "Leets",
+          description:
+            "Condividi le tue scoperte musicali e promuovi artisti emergenti",
+          images: [
+            {
+              url: "https://beta.leets.it/logo.png",
+              width: 800,
+              height: 600,
+              alt: "Leets logo",
+              type: "image/png",
+            },
+          ],
+          site_name: "Leets",
+        }}
+      />
+      <Feed />
+    </>
+  );
 }
