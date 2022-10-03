1704877f469e22be16325cb95b75b0e2ee73de12
mattiapomelli <mattiapomelli@gmail.com>
2022-10-03 21:22:08 +0200

    Create Tabs component

---
+++ src/components/Basic/Tabs.tsx
@@ -0,0 +1,48 @@
+import { Tab } from "@headlessui/react";
+import cx from "classnames";
+import { Fragment, ReactNode } from "react";
+
+interface TabItem {
+  label: string;
+  content: ReactNode;
+}
+
+interface TabsProps {
+  items: TabItem[];
+  className?: string;
+}
+
+export const Tabs = ({ items, className }: TabsProps) => {
+  return (
+    <Tab.Group>
+      <Tab.List className={cx("flex gap-2", className)}>
+        {items.map((item) => (
+          <Tab key={item.label} as={Fragment}>
+            {({ selected }) => (
+              <button
+                className={cx(
+                  "rounded-btn",
+                  "text-sm font-medium",
+                  "py-2.5 min-w-[140px]",
+                  "focus:outline-none",
+                  "focus-visible:ring-4",
+                  "focus:ring-primary focus:ring-opacity-50",
+                  selected
+                    ? "bg-secondary text-secondary-content shadow"
+                    : "bg-base-200 text-text-primary hover:opacity-80"
+                )}
+              >
+                {item.label}
+              </button>
+            )}
+          </Tab>
+        ))}
+      </Tab.List>
+      <Tab.Panels className="mt-4">
+        {items.map((item) => (
+          <Tab.Panel key={item.label}>{item.content}</Tab.Panel>
+        ))}
+      </Tab.Panels>
+    </Tab.Group>
+  );
+};
--- src/pages/[username].tsx
+++ src/pages/[username].tsx
@@ -1,11 +1,10 @@
-import { Tab } from "@headlessui/react";
 import { useLingui } from "@lingui/react";
-import cx from "classnames";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { Fragment, ReactNode } from "react";
 
 import { ApiClient } from "@api/client";
 import { User } from "@api/users";
+import { Tabs } from "@components/Basic/Tabs";
 import { UserHuntedSongs } from "@components/UserHuntedSongs";
 import { UserLikedSongs } from "@components/UserLikedSongs";
 import { PageWithLayout } from "@types";
@@ -18,50 +17,29 @@ interface TabItem {
 const UserPage: PageWithLayout<{ user: User }> = ({ user }) => {
   const { i18n } = useLingui();
 
-  const tabs: TabItem[] = [
+  const tabItems: TabItem[] = [
     {
       label: i18n._("Liked"),
-      content: <UserLikedSongs user={user} />,
+      content: (
+        <div className="mt-8">
+          <UserLikedSongs user={user} />
+        </div>
+      ),
     },
     {
       label: i18n._("Hunted"),
-      content: <UserHuntedSongs user={user} />,
+      content: (
+        <div className="mt-8">
+          <UserHuntedSongs user={user} />
+        </div>
+      ),
     },
   ];
 
   return (
     <>
       <h3 className="mt-8 mb-6 text-2xl font-bold">{user.username}</h3>
-      <Tab.Group>
-        <Tab.List className="flex gap-2">
-          {tabs.map((tab) => (
-            <Tab key={tab.label} as={Fragment}>
-              {({ selected }) => (
-                <button
-                  className={cx(
-                    "rounded-btn",
-                    "text-sm font-medium",
-                    "py-2.5 min-w-[140px]",
-                    "focus:outline-none",
-                    "focus-visible:ring-4",
-                    "focus:ring-primary focus:ring-opacity-50",
-                    selected
-                      ? "bg-secondary text-secondary-content shadow"
-                      : "bg-base-200 text-text-primary hover:opacity-80"
-                  )}
-                >
-                  {tab.label}
-                </button>
-              )}
-            </Tab>
-          ))}
-        </Tab.List>
-        <Tab.Panels className="mt-8">
-          {tabs.map((tab) => (
-            <Tab.Panel key={tab.label}>{tab.content}</Tab.Panel>
-          ))}
-        </Tab.Panels>
-      </Tab.Group>
+      <Tabs items={tabItems} />
     </>
   );
 };
