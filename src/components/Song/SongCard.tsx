162a8d1cd4c9777ac27b560e786cf618add209db
mattiapomelli <mattiapomelli@gmail.com>
2022-10-03 20:51:02 +0200

    Add hunter to song card

--- src/components/Song/SongCard.tsx
+++ src/components/Song/SongCard.tsx
@@ -1,6 +1,7 @@
 import { Trans } from "@lingui/macro";
 import { useMutation } from "@tanstack/react-query";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { FaSpotify } from "react-icons/fa";
 
@@ -45,73 +46,94 @@ export const SongCard = ({ post, onLikeChange }: SongCardProps) => {
   };
 
   return (
-    <div className="rounded-btn relative flex w-full gap-3 bg-secondary p-2.5 text-secondary-content xs:gap-4 xs:p-3">
-      {/* Spotify Icon */}
-      <a
-        href={`https://open.spotify.com/track/${post.spotify_id}`}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="absolute top-2 right-2 cursor-pointer"
-      >
-        <FaSpotify color="#050e1d" className="h-5 w-5 xs:h-6 xs:w-6" />
-      </a>
-      {/* Song Image */}
-      {post.image && (
-        <div className="relative hidden h-24 w-24 shrink-0 xs:block xs:h-28 xs:w-28 [@media(min-width:320px)]:block">
-          <Image
-            className="rounded-btn h-full object-cover"
-            src={post.image}
-            alt="song"
-            layout="fill"
-          />
+    <div className="rounded-btn relative flex w-full flex-col justify-between gap-3 bg-secondary p-2.5 text-secondary-content sm:flex-row xs:gap-4 xs:p-3">
+      <div className="flex min-w-0 gap-3">
+        {/* Song Image */}
+        {post.image && (
+          <div className="relative hidden h-24 w-24 shrink-0 xs:block xs:h-28 xs:w-28 [@media(min-width:320px)]:block">
+            <Image
+              className="rounded-btn h-full object-cover"
+              src={post.image}
+              alt="song"
+              layout="fill"
+            />
+          </div>
+        )}
+
+        {/* Song Details */}
+        <div className="flex w-full min-w-0 flex-col justify-center xs:gap-1">
+          <span className="text-xs font-bold uppercase">
+            {post.artist.name}
+          </span>
+          <span className="truncate text-xl font-bold">{post.title}</span>
+          <div className="flex items-center gap-2">
+            {post.preview_url ? (
+              <Player
+                id={post.spotify_id}
+                previewTrackUrl={post.preview_url}
+                className="-ml-1"
+                playerClassName="w-9 h-9 xs:w-10 xs:h-10"
+              />
+            ) : (
+              <a
+                href={`https://open.spotify.com/track/${post.spotify_id}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button
+                  leftIcon={<FaSpotify size={18} />}
+                  size="xs"
+                  className="mt-1 px-2"
+                >
+                  <Trans>Listen on Spotify</Trans>
+                </Button>
+              </a>
+            )}
+          </div>
         </div>
-      )}
-      {/* Song Details */}
-      <div className="flex w-full min-w-0 flex-col justify-center xs:gap-1">
-        <span className="text-xs font-bold uppercase">{post.artist.name}</span>
-        <span className="truncate text-xl font-bold">{post.title}</span>
-        {post.preview_url ? (
-          <Player
-            id={post.spotify_id}
-            previewTrackUrl={post.preview_url}
-            className="-ml-1"
-            playerClassName="w-9 h-9 xs:w-10 xs:h-10"
-          />
-        ) : (
+      </div>
+
+      <div className="flex items-center justify-between sm:flex-col sm:items-end">
+        {/* Hunter */}
+        <div className="sm:text-right">
+          <p className="text-xs leading-3">Hunted by</p>
+          <Link href={`/${post.hunter.username}`}>
+            <a className="font-bold hover:text-secondary-content/60">
+              @{post.hunter.username}
+            </a>
+          </Link>
+        </div>
+
+        <div className="flex items-center gap-4">
+          {/* Spotify Icon */}
           <a
             href={`https://open.spotify.com/track/${post.spotify_id}`}
             target="_blank"
             rel="noopener noreferrer"
+            className="cursor-pointer"
           >
-            <Button
-              leftIcon={<FaSpotify size={18} />}
-              size="xs"
-              className="mt-1 px-2"
-            >
-              <Trans>Listen on Spotify</Trans>
-            </Button>
+            <FaSpotify color="#050e1d" className="h-5 w-5" />
           </a>
-        )}
-      </div>
-      {/* Likes */}
-      <div className="absolute bottom-2 right-2">
-        {post.status === "UPLOADED" && (
-          <span className="top-0 right-0 z-10 rounded-lg bg-info py-0.5 px-1.5 text-sm">
-            <Trans>Under review</Trans>
-          </span>
-        )}
-        {post.status === "ONLINE" && (
-          <div className="flex cursor-pointer items-center gap-1">
-            <button onClick={toggleLike} className="cursor-pointer">
-              {post.isLiked ? (
-                <HeartSolid className="text-2xl" />
-              ) : (
-                <HeartOutline className="text-2xl" />
-              )}
-            </button>
-            <span className="text-lg">{post.likes}</span>
-          </div>
-        )}
+
+          {/* Status / Likes */}
+          {post.status === "UPLOADED" && (
+            <span className="top-0 right-0 z-10 rounded-lg bg-info py-0.5 px-1.5 text-sm">
+              <Trans>Under review</Trans>
+            </span>
+          )}
+          {post.status === "ONLINE" && (
+            <div className="flex cursor-pointer items-center gap-1">
+              <button onClick={toggleLike} className="cursor-pointer">
+                {post.isLiked ? (
+                  <HeartSolid className="text-2xl" />
+                ) : (
+                  <HeartOutline className="text-2xl" />
+                )}
+              </button>
+              <span className="text-lg">{post.likes}</span>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
