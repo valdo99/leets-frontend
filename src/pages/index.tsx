e1317d9343fad6497fccbe2e09c60f0903b2fd4b
valdozzz <edvaldo99@icloud.com>
2022-10-06 19:50:47 +0200

    hot fix head not used

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import { NextSeo } from "next-seo";
-import Head from "next/head";
 
 import { TopHuntersFeed } from "@components/TopHuntersFeed";
 import { TopSongsFeed } from "@components/TopSongsFeed";
