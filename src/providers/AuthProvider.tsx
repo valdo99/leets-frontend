9f8029fe04e408f35ac7f7e0d37c6bff289e0c1c
mattiapomelli <mattiapomelli@gmail.com>
2022-10-08 13:31:57 +0200

    Add language header to api client

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,3 +1,4 @@
+import { useLingui } from "@lingui/react";
 import { useAtom } from "jotai";
 import { createContext, ReactNode, useContext, useEffect, useRef } from "react";
 import { toast } from "react-toastify";
@@ -6,6 +7,7 @@ import { ForbiddenError } from "@api/errors";
 import { LoginModal } from "@components/Modals/LoginModal";
 import { loginModalAtom } from "@state/loginModal";
 import { userAtom } from "@state/user";
+import { Locale } from "locales/available-locales";
 
 import { ApiClient } from "../api/client";
 
@@ -14,6 +16,8 @@ const ApiClientContext = createContext<ApiClient | undefined>(undefined);
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [, setUserState] = useAtom(userAtom);
   const [showLoginModal, setShowLoginModal] = useAtom(loginModalAtom);
+
+  const { i18n } = useLingui();
 
   const handleUnauthorizedError = async () => {
     setUserState({
@@ -32,6 +36,10 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
       handleForbiddenError,
     })
   );
+
+  useEffect(() => {
+    apiClientRef.current.setLocaleHeader(i18n.locale as Locale);
+  }, [i18n.locale]);
 
   useEffect(() => {
     const getLoggedUser = async () => {
