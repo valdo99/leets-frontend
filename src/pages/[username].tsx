9834df6106bfab6c2dc0372f6733c0139111b155
valdozzz <edvaldo99@icloud.com>
2022-10-25 22:30:29 +0200

    hot fix update username

--- src/pages/[username].tsx
+++ src/pages/[username].tsx
@@ -1,5 +1,6 @@
 import { t } from "@lingui/macro";
 import { useLingui } from "@lingui/react";
+import { useAtom } from "jotai";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { NextSeo } from "next-seo";
 import { useRouter } from "next/router";
@@ -13,6 +14,7 @@ import { EditUsernameModal } from "@components/Modals/EditUsernameModal";
 import { UserHuntedSongs } from "@components/UserHuntedSongs";
 import { UserLikedSongs } from "@components/UserLikedSongs";
 import EditIcon from "@icons/edit.svg";
+import { userAtom } from "@state/user";
 import { PageWithLayout } from "@types";
 
 import SEO from "../../next-seo.config";
@@ -20,6 +22,7 @@ import SEO from "../../next-seo.config";
 const UserPageInner = ({ user }: { user: User }) => {
   const { i18n } = useLingui();
   const [showUpdateModal, setShowUpdateModal] = useState<boolean>(false);
+  const [{ user: loggedUser }] = useAtom(userAtom);
 
   const tabItems: TabItem[] = [
     {
@@ -45,9 +48,11 @@ const UserPageInner = ({ user }: { user: User }) => {
       <NextSeo {...SEO} title={`Leets | ${user.username}`} />
       <div className="mt-8 mb-6 flex items-center gap-2">
         <h3 className="text-2xl font-bold md:text-3xl">{user.username}</h3>
-        <button onClick={() => setShowUpdateModal(true)}>
-          <EditIcon className="h-6 w-6" />
-        </button>
+        {user.username === loggedUser?.username && (
+          <button onClick={() => setShowUpdateModal(true)}>
+            <EditIcon className="h-6 w-6" />
+          </button>
+        )}
       </div>
 
       <Tabs items={tabItems} />
