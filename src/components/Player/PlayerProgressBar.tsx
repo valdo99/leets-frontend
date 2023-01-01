a469086b798dd82cea617699b01bb9c2486b2a8b
mattiapomelli <mattiapomelli@gmail.com>
2023-01-01 17:47:31 +0100

    Allow to move song forward in time

--- src/components/Player/PlayerProgressBar.tsx
+++ src/components/Player/PlayerProgressBar.tsx
@@ -1,3 +1,4 @@
+import * as SliderPrimitive from "@radix-ui/react-slider";
 import cx from "classnames";
 import { useEffect, useState } from "react";
 
@@ -9,15 +10,14 @@ interface PlayerProgressBarProps {
 
 export const PlayerProgressBar = ({ className }: PlayerProgressBarProps) => {
   const { isPlaying, song, audio } = usePlayer();
-  const [percentagePlayed, setPercentagePlayed] = useState(0);
+  const [timePlayed, setTimePlayed] = useState(0);
 
   useEffect(() => {
     if (!isPlaying) return;
 
     const intervalId = setInterval(() => {
       if (!audio) return;
-
-      setPercentagePlayed((audio.currentTime / audio.duration) * 100);
+      setTimePlayed(audio.currentTime);
     }, 100);
 
     return () => {
@@ -25,17 +25,32 @@ export const PlayerProgressBar = ({ className }: PlayerProgressBarProps) => {
     };
   }, [isPlaying, audio, song]);
 
+  const onTimeChange = (value: number[]) => {
+    if (!audio) return;
+    setTimePlayed(value[0]);
+    audio.currentTime = value[0];
+  };
+
   return (
-    <div
-      className={cx(
-        "h-[0.4rem] w-[16rem] lg:w-[24rem] rounded-full bg-secondary",
-        className
-      )}
-    >
-      <div
-        className="h-full rounded-full bg-primary"
-        style={{ width: `${percentagePlayed}%` }}
-      />
+    <div className={className}>
+      <SliderPrimitive.Root
+        value={[timePlayed]}
+        onValueChange={onTimeChange}
+        defaultValue={[0]}
+        max={audio?.duration}
+        aria-label="Time"
+        className="group relative flex h-[0.4rem] w-[16rem] cursor-pointer touch-none items-center lg:w-[24rem]"
+      >
+        <SliderPrimitive.Track className="relative h-1 w-full grow rounded-full bg-secondary">
+          <SliderPrimitive.Range className="absolute h-full rounded-full bg-primary" />
+        </SliderPrimitive.Track>
+        <SliderPrimitive.Thumb
+          className={cx(
+            "hidden group-hover:block h-4 w-4 rounded-full bg-primary",
+            "focus:outline-none focus-visible:ring focus-visible:ring-primary focus-visible:ring-opacity-50"
+          )}
+        />
+      </SliderPrimitive.Root>
     </div>
   );
 };
